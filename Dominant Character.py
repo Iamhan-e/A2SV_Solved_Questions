1391b5b656d691100639a09010ed9826b2acf416
Iamhan-e <h2anaschalew@gmail.com>
2026-02-12 22:40:20 +0300

    fixed bug

--- Dominant Character.py
+++ Dominant Character.py
@@ -1,27 +1,30 @@
 import sys
-from collections import Counter
 
-input= sys.stdin.readline
 
-t= int(input())
+input_data= sys.stdin.read().split()
 
-for _ in range(t):
-    l= int(input())
-    strs= input()
-    
-    if "aa" in strs:
-        print(2)
+if input_data:
+    t= int(input_data[0])
+    i=1
+
+    for _ in range(t):
+        l= int(input_data[i])
+        strs= input_data[i+1]
+        i+=2
         
-    elif "aba" in strs or "aca" in strs:
-        print(3)
-        
-    elif "abca" in strs or "acba" in strs:
-        print(4)
-        
+        if "aa" in strs:
+            print(2)
+            
+        elif "aba" in strs or "aca" in strs:
+            print(3)
+            
+        elif "abca" in strs or "acba" in strs:
+            print(4)
+            
 
-    elif "abcabca" in strs or "acbacba" in strs:
-        print(7)
-    
-    else:   
+        elif "abbacca" in strs or "accabba" in strs:
+            print(7)
+        
+        else:   
 
-        print (-1)
+            print (-1)
