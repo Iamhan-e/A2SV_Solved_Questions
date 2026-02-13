aff951051fcd28f21a9b3e70e97f0338f110e903
Iamhan-e <h2anaschalew@gmail.com>
2026-02-13 15:09:20 +0300

    improved the code

--- valid pair.py
+++ valid pair.py
@@ -3,15 +3,9 @@ class Solution:
     def findValidPair(self, s: str) -> str:
         
         cnt= Counter(s)
-        
 
         for i in range(len(s)-1):
-            first= s[i]
-            second= s[i+1]
-
-            if first != second:
-
-                if str(cnt[first]) == first and str(cnt[second]) == second:
-                    return first+second
+            if s[i] != s[i+1] and str(cnt[s[i]]) == s[i] and str(cnt[s[i+1]]) == s[i+1]:
+                return s[i]+s[i+1]
 
         return ""
