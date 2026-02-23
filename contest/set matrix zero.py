c3848f3694f2b823efdfd1b5c23bb9ba208a5d96
Iamhan-e <h2anaschalew@gmail.com>
2026-02-23 12:55:32 +0300

    73. Set Matrix Zeroes

---
+++ contest/set matrix zero.py
@@ -0,0 +1,28 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])
+    
+        row= set()
+        col= set()
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]== 0:
+                    row.add(i)
+                    col.add(j)
+        
+        for r in row:
+            for i in range(n):
+                matrix[r][i]= 0
+
+        for c in col:
+            for i in range(m):
+                matrix[i][c]= 0
+
+        return matrix
+            
+
+        
