1d0c82f6f9778fe882874bac752a533f86bfbad9
Iamhan-e <h2anaschalew@gmail.com>
2026-03-09 23:48:47 +0300

    improved the answer

--- needle_haystack.py
+++ needle_haystack.py
@@ -1,49 +1,78 @@
 import sys
-input = sys.stdin.readline
+from collections import Counter
 
-t = int(input())
-
-for _ in range(t):
-    s = input().strip()
-    t_str = input().strip()
-
-    cs = [0]*26
-    ct = [0]*26
-
-    for c in s:
-        cs[ord(c)-97] += 1
-    for c in t_str:
-        ct[ord(c)-97] += 1
-
-    possible = True
-    for i in range(26):
-        if cs[i] > ct[i]:
-            possible = False
-            break
-
-    if not possible:
-        print("Impossible")
-        continue
-
-    rem = [ct[i] - cs[i] for i in range(26)]
-    first = ord(s[0]) - 97
-
+def main():
+    data = sys.stdin.read().splitlines()
+    t = int(data[0])
+    index = 1
+    out_lines = []
     
-    a = []
-    for i in range(first):
-        a.append(chr(i+97)*rem[i])
-    a.append(s)
-    for i in range(first,26):
-        a.append(chr(i+97)*rem[i])
-    cand1 = "".join(a)
+    for _ in range(t):
+        s = data[index].strip()
+        index += 1
+        t_str = data[index].strip()
+        index += 1
+        
+        s_count = Counter(s)
+        t_count = Counter(t_str)
+        possible = True
+        for char in s_count:
+            if s_count[char] > t_count.get(char, 0):
+                out_lines.append("Impossible")
+                possible = False
+                break
+                
+        if not possible:
+            continue
+            
+        n = len(s)
+        suffix = [[0] * 26 for _ in range(n + 1)]
+        
+        for i in range(n - 1, -1, -1):
+            for j in range(26):
+                suffix[i][j] = suffix[i + 1][j]
+            char_index = ord(s[i]) - ord('a')
+            suffix[i][char_index] += 1
+            
+        current_counts = [0] * 26
+        for i in range(26):
+            char = chr(ord('a') + i)
+            current_counts[i] = t_count.get(char, 0)
+            
+        s_pos = 0
+        res = []
+        len_t = len(t_str)
+        
+        for _ in range(len_t):
+            found = False
+            for c_idx in range(26):
+                if current_counts[c_idx] == 0:
+                    continue
+                new_s_pos = s_pos
+                if s_pos < n and s[s_pos] == chr(ord('a') + c_idx):
+                    new_s_pos += 1
+                valid = True
+                for d_idx in range(26):
+                    required = suffix[new_s_pos][d_idx]
+                    available = current_counts[d_idx]
+                    if d_idx == c_idx:
+                        available -= 1
+                    if available < required:
+                        valid = False
+                        break
+                if valid:
+                    res.append(chr(ord('a') + c_idx))
+                    current_counts[c_idx] -= 1
+                    s_pos = new_s_pos
+                    found = True
+                    break
+            if not found:
+                out_lines.append("Impossible")
+                break
+        else:
+            out_lines.append(''.join(res))
+            
+    print("\n".join(out_lines))
 
-    
-    b = []
-    for i in range(first+1):
-        b.append(chr(i+97)*rem[i])
-    b.append(s)
-    for i in range(first+1,26):
-        b.append(chr(i+97)*rem[i])
-    cand2 = "".join(b)
-
-    print(min(cand1, cand2))
+if __name__ == "__main__":
+    main()
