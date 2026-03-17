5ad293b83c7c64aef94fd431d788a17815af2852
Iamhan-e <h2anaschalew@gmail.com>
2026-03-17 22:53:59 +0300

    imporved code

--- longest_substring.py
+++ longest_substring.py
@@ -1,16 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        freqs = defaultdict(int)
-        res = 0
-        i = 0
-
-        for j in range(len(s)):
-            freqs[s[j]] += 1
-            maxFreq = max(freqs.values())
-            curLen = j - i + 1
-            if curLen - maxFreq > k:
-                freqs[s[i]] -= 1
-                i += 1
-            res = max(res, j - i + 1)
+        freq= defaultdict(int)
+        max_cnt=0
+        res=0
+        i=0
         
+        for j in range(len(s)):
+            
+            freq[s[j]]+=1
+            max_cnt= max(max_cnt, freq[s[j]])
+
+            while (j-i+1) - max_cnt >k :
+                
+                freq[s[i]]-=1
+                i+=1
+            res= max(res, j-i+1)
+            
         return res
