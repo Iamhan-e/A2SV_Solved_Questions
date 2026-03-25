98cf4606f533821e82f9ee14ab8d49878bd54758
Iamhan-e <h2anaschalew@gmail.com>
2026-03-26 00:43:07 +0300

    updated

--- Prefix Sum/continuous_subarray.py
+++ Prefix Sum/continuous_subarray.py
@@ -1,12 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        remainder_cache = {0:-1}
-        remainder = 0
-        for i in range(len(nums)):
-            remainder += nums[i]
-            remainder %=k
-            if remainder not in remainder_cache:
-                remainder_cache[remainder] = i
-            elif i - remainder_cache[remainder] >=2:
+        remainder= {0:-1}
+
+        total=0
+
+        for i, n in enumerate(nums):
+            total+=n
+            r= total % k
+
+            if r not in remainder:
+                remainder[r]= i
+            
+            elif i - remainder[r] > 1:
                 return True
         return False
