055041193136df69996d1abbae602d2d0521c6e4
Iamhan-e <h2anaschalew@gmail.com>
2026-03-27 21:44:59 +0300

    Subsets

---
+++ Recursion and Backtracking/subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res, subset= [], []
+
+        def backtrack(i):
+
+            if i== len(nums):
+                return res.append(subset[:])
+
+            backtrack(i+1)
+            subset.append(nums[i])
+            backtrack(i+1)
+            subset.pop()
+
+        backtrack(0)
+        return res
