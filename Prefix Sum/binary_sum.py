0f02c02041cda83af7bd2fda800e1c23ea78472a
Iamhan-e <h2anaschalew@gmail.com>
2026-03-30 13:58:32 +0300

    Binary Subarrays With Sum

---
+++ Prefix Sum/binary_sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        prefix_sum= 0
+        pre_freq= {0:1}
+        cnt=0
+
+        for n in nums:
+            prefix_sum+= n
+
+            if prefix_sum - goal in pre_freq:
+                cnt+= pre_freq[prefix_sum - goal]
+            
+            pre_freq[prefix_sum]= pre_freq.get(prefix_sum, 0)+ 1
+        return cnt
