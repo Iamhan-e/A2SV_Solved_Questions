8bb1e8e8e671d666887033adb2a10c21dd909c37
Iamhan-e <h2anaschalew@gmail.com>
2026-03-30 13:26:37 +0300

    optimized it

--- Prefix Sum/shifting_elements.py
+++ Prefix Sum/shifting_elements.py
@@ -1,20 +1,23 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         
-        n = len(s)
-        shift = [0] * (n + 1)
+        n= len(s)
+        diff= [0] * (n+1)
 
-        for shiftOp in shifts:
-            start, end, direction = shiftOp
-            shift[start] += (1 if direction == 1 else -1)
-            if end + 1 < n:
-                shift[end + 1] -= (1 if direction == 1 else -1)
+        for start, end, direction in shifts:
 
-        currentShift = 0
-        shiftList = list(s)
+            if direction == 0:
+                diff[start] -=1
+                diff[end+1]+=1
+            
+            else:
+                diff[start] +=1
+                diff[end+1]-=1
+        
+        curr= 0
+        shift_str= ''
         for i in range(n):
-            currentShift += shift[i]
-            netShift = (currentShift % 26 + 26) % 26
-            shiftList[i] = chr((ord(shiftList[i]) - ord('a') + netShift) % 26 + ord('a'))
+            curr+= diff[i]
 
-        return ''.join(shiftList)
+            shift_str+= chr(((ord(s[i]) - ord('a')+ curr) % 26) + ord('a'))
+        return shift_str
