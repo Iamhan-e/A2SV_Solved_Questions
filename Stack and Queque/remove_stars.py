4c54ba190a1a5b35dddd657db9aeeadce029bd06
Iamhan-e <h2anaschalew@gmail.com>
2026-04-03 23:40:53 +0300

    fixed typo

--- Stack and Queque/remove_stars.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeStars(self, s: str) -> str:
-        stack = []
-        for i in s:
-            if stack and i == "*":
-                stack.pop()
-            else:
-                stack.append(i)
-        return "".join(stack)
