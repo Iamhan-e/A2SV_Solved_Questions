c407de274a8624f3f49a8d86fa8f838492b13888
Iamhan-e <h2anaschalew@gmail.com>
2026-04-05 23:30:04 +0300

    1598. Crawler Log Folder

---
+++ Stack and Queue/crawler_log.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack= []
+
+        for path in logs:
+            if path == '../':
+                if stack:
+                    stack.pop()
+            elif path == './' :
+                continue
+            else:
+                stack.append(path)
+        
+        return len(stack)
