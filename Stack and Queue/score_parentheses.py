b489c0f7f7b21b9eff266523e5535f8f18d0a12b
Iamhan-e <h2anaschalew@gmail.com>
2026-04-24 15:51:09 +0300

    improved the code

--- Stack and Queue/score_parentheses.py
+++ Stack and Queue/score_parentheses.py
@@ -1,19 +1,23 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        
-        
-        stack = [0]  
-        
-        for ch in s:
-            if ch == '(':
-                stack.append(0)     
-            else:  
-                top = stack.pop()    
-                if top == 0:        
-                    score = 1
-                else:                
-                    score = 2 * top
+        stack= []
+        total= 0
+        for i in range(len(s)):
+
+            if s[i] == '(':
+                stack.append(s[i])
+
+            else:
+                if stack and stack[-1] == '(' :
+                    
+                    stack.pop()
+                    stack.append(1)
+                else:
+                    score= 0
+                    while stack and isinstance(stack[-1], int):
+                        score+= stack.pop()
+                    stack.pop()
+                    stack.append(2 * score)
+
                 
-                stack[-1] += score
-        
-        return stack[0]
+        return sum(stack)
